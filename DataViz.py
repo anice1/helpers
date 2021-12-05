152b66cef75a9b3d781d5df35eabb23f8169c3ad
anochima <chimaobinice@gmail.com>
2021-12-05 20:17:09 +0100

    Updated the ImageProcessor library

--- DataViz.py
+++ DataViz.py
@@ -4,178 +4,171 @@ import numpy as np
 import itertools
 
 
-class DataViz:
+def plot(x, y, title, xlabel, ylabel, legend):
+    plt.plot(x, y, 'o')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(legend)
+    plt.show()
+
+def scatter(x, y, title, xlabel, ylabel, legend):
+    plt.scatter(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(legend)
+    plt.show()
+
+def hist(x, title, xlabel, ylabel):
+    plt.hist(x)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+def box( x, title, xlabel, ylabel):
+    plt.boxplot(x)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+def violin( x, title, xlabel, ylabel):
     """
-        A tiny class to visualize data
+    Plots a violin chart
+    Args:
+        x ([type]): [description]
+        title ([type]): [description]
+        xlabel ([type]): [description]
+        ylabel ([type]): [description]
     """
-    def __init__(self, data):
-        self.data = data
+    plt.violinplot(x)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
 
-    def plot(self, x, y, title, xlabel, ylabel, legend):
-        plt.plot(x, y, 'o')
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.legend(legend)
-        plt.show()
+def bar( x, title, xlabel, ylabel):
+    """[Plots a bar chart]
+    Args:
+        x ([type]): [description]
+        title ([type]): [description]
+        xlabel ([type]): [description]
+        ylabel ([type]): [description]
+    """
+    plt.bar(x)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
 
-    def scatter(self, x, y, title, xlabel, ylabel, legend):
-        plt.scatter(x, y)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.legend(legend)
-        plt.show()
-
-    def hist(self, x, title, xlabel, ylabel):
-        plt.hist(x)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-
-    def box(self, x, title, xlabel, ylabel):
-        plt.boxplot(x)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-
-    def violin(self, x, title, xlabel, ylabel):
-        """
-        Plots a violin chart
-        Args:
-            x ([type]): [description]
-            title ([type]): [description]
-            xlabel ([type]): [description]
-            ylabel ([type]): [description]
-        """
-        plt.violinplot(x)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-
-    def bar(self, x, title, xlabel, ylabel):
-        """[Plots a bar chart]
-        Args:
-            x ([type]): [description]
-            title ([type]): [description]
-            xlabel ([type]): [description]
-            ylabel ([type]): [description]
-        """
-        plt.bar(x)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.show()
-
-    def pie(self, x, title, xlabel, ylabel):
-        """[Plots a pie chart]
-        Args:
-            x ([type]): [description]
-            title ([type]): [description]
-            xlabel ([type]): [description]
-            ylabel ([type]): [description]
-        """
-        plt.pie(x)
-        plt.title(title)
-        
-    def plot_decision_boundary(self):
-        """Plots a decision boundary
-        """
-        pass
+def pie( x, title, xlabel, ylabel):
+    """[Plots a pie chart]
+    Args:
+        x ([type]): [description]
+        title ([type]): [description]
+        xlabel ([type]): [description]
+        ylabel ([type]): [description]
+    """
+    plt.pie(x)
+    plt.title(title)
     
-    def plot_history(self, history):
-        """
-        Returns a plots of model learning history object
+def plot_decision_boundary():
+    """Plots a decision boundary
+    """
+    pass
 
-        Args:
-            history: Model's learning history object
-        
-        Returns:
-            A plot of of model learning history
-        """
+def plot_history( history):
+    """
+    Returns a plots of model learning history object
 
-        train_loss = history.history['loss']
-        val_loss = history.history['val_loss']
-
-        train_accuracy = history.history['accuracy']
-        val_accuracy = history.history['val_accuracy']
-
-        # Plot the histories
-        plt.plot(train_loss, label="Train Loss")
-        plt.plot(val_loss, label="Val Loss")
-        plt.xlabel('Epochs')
-        plt.legend()
-
-        # Plot the accuracy
-        plt.figure()
-        plt.plot(train_accuracy, label="Train Accuracy")
-        plt.plot(val_accuracy, label="Val Accuracy")
-        plt.xlabel('Epochs')
-        plt.legend()
+    Args:
+        history: Model's learning history object
     
+    Returns:
+        A plot of of model learning history
+    """
 
-    def plot_lr_vs_loss(self,learning_rate, history):
-        """
-        Plot a semiglogx chart showing the model's learning rate vs. loss performance
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    train_accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    # Plot the histories
+    plt.plot(train_loss, label="Train Loss")
+    plt.plot(val_loss, label="Val Loss")
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot the accuracy
+    plt.figure()
+    plt.plot(train_accuracy, label="Train Accuracy")
+    plt.plot(val_accuracy, label="Val Accuracy")
+    plt.xlabel('Epochs')
+    plt.legend()
+
+
+def plot_lr_vs_loss(learning_rate, history):
+    """
+    Plot a semiglogx chart showing the model's learning rate vs. loss performance
+    Args:
+        learning_rate: the learning rate callback
+        history: model training history example(history = model.fit(X,y, epochs=100))
+    """
+    plt.figure(figsize=(10, 7))
+    plt.semilogx(learning_rate, history.history['loss'])
+    plt.xlabel('Learning Rate')
+    plt.ylabel('Loss')
+    plt.title('Learning Rate Vs. Loss')
+    plt.show()
+
+
+def plot_conf_matrix(conf_matrix, classes=None, figsize=(10,7), text_size=10):
+        """Plots a confusion matrix chart
+
         Args:
-            learning_rate: the learning rate callback
-            history: model training history example(history = model.fit(X,y, epochs=100))
+            conf_matrix (array): confusion matrix
         """
-        plt.figure(figsize=self.figsize)
-        plt.semilogx(learning_rate, history.history['loss'])
-        plt.xlabel('Learning Rate')
-        plt.ylabel('Loss')
-        plt.title('Learning Rate Vs. Loss')
-        plt.show()
-
-
-    def plot_conf_matrix(conf_matrix, classes=None, figsize=(10,7), text_size=10):
-            """Plots a confusion matrix chart
-
-            Args:
-                conf_matrix (array): confusion matrix
-            """
-            fig, ax = plt.subplots(figsize=figsize)
+        fig, ax = plt.subplots(figsize=figsize)
+        
+        # Normalize our confusion matrix
+        conf_norm = conf_matrix.astype('float') / conf_matrix.sum(axis=-1)[:np.newaxis]
+        n_classes = conf_norm.shape[0]
+        
+        # Create matrix plot
+        matshow = ax.matshow(conf_matrix, cmap=plt.cm.Blues)
+        fig.colorbar(matshow)
+        
+        # Check classes
+        if classes:
+            labels = classes
+        else:
+            labels = np.arange(conf_matrix.shape[0])
             
-            # Normalize our confusion matrix
-            conf_norm = conf_matrix.astype('float') / conf_matrix.sum(axis=-1)[:np.newaxis]
-            n_classes = conf_norm.shape[0]
-            
-            # Create matrix plot
-            matshow = ax.matshow(conf_matrix, cmap=plt.cm.Blues)
-            fig.colorbar(matshow)
-            
-            # Check classes
-            if classes:
-                labels = classes
-            else:
-                labels = np.arange(conf_matrix.shape[0])
-                
-            # Label the classes
-            ax.set(title='Confusion Matrix',
-                xlabel='Predicted Label',
-                ylabel='True Label',
-                xticks=labels,
-                yticks=labels)
-            
-            # Set x-axis labels to bottom
-            ax.xaxis.set_label_position('bottom')
-            ax.xaxis.tick_bottom()
-            
-            #Adjust label size
-            ax.yaxis.label.set_size(text_size)
-            ax.yaxis.label.set_size(text_size)
-            ax.title.set_size(text_size)
-            
-            # Set the threshold for different colors
-            threshold = (conf_matrix.max() + conf_matrix.min()) / 2
-            
-            # Plot text on each cell
-            for i, j in itertools.product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
-                plt.text(j, i, f"{conf_matrix[i, j]} ({conf_norm[i,j]*100:.1f}%)", 
-                        horizontalalignment="center",
-                        color="white" if conf_matrix[i,j] > threshold else "black", size=15)
+        # Label the classes
+        ax.set(title='Confusion Matrix',
+            xlabel='Predicted Label',
+            ylabel='True Label',
+            xticks=labels,
+            yticks=labels)
+        
+        # Set x-axis labels to bottom
+        ax.xaxis.set_label_position('bottom')
+        ax.xaxis.tick_bottom()
+        
+        #Adjust label size
+        ax.yaxis.label.set_size(text_size)
+        ax.yaxis.label.set_size(text_size)
+        ax.title.set_size(text_size)
+        
+        # Set the threshold for different colors
+        threshold = (conf_matrix.max() + conf_matrix.min()) / 2
+        
+        # Plot text on each cell
+        for i, j in itertools.product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
+            plt.text(j, i, f"{conf_matrix[i, j]} ({conf_norm[i,j]*100:.1f}%)", 
+                    horizontalalignment="center",
+                    color="white" if conf_matrix[i,j] > threshold else "black", size=15)
         
--- ImageProcessor.py
+++ ImageProcessor.py
@@ -4,85 +4,82 @@ import os
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import random
-import pdf2image as p2i
 import requests
 
-class ImageProcessor:
-    def __init__(self):
-        self.img = None
-        self.IMG_HEIGHT = 255
-        self.IMG_WIDTH = 255
+img = None
+IMG_HEIGHT = 255
+IMG_WIDTH = 255
     
-    def load_image(self, img_path):
-        self.img = mpimg.imread(img_path)
-        return self.img
+def load_image( img_path):
+    img = mpimg.imread(img_path)
+    return img
+
+def process_images( img_folder):
+    """Processes image required for feeding into a neural network
+
+    Args:
+        img_folder (string): The folder containing the images to be processed.
+    """
+    image_data_array =[]
+    class_name = []
     
-    def process_images(self, img_folder):
-        """Processes image required for feeding into a neural network
+    #Loop through the image folder, get, resize and normalize image
+    for directory in os.listdir(img_folder):
+        for file_path in os.listdir(os.path.join(img_folder, directory)):
+            image_path = os.path.join(img_folder, directory, file_path) #Initialize the image path
+            # Convert Image to grayscale
+            image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
+            #Resize the image
+            if(image.any()):
+                image = cv2.resize(image, (255, 255), interpolation = cv2.INTER_AREA)
+                #Transform image into a tensor
+                image = np.array(image)
+                image = image.astype('float32')
 
-        Args:
-            img_folder (string): The folder containing the images to be processed.
-        """
-        image_data_array =[]
-        class_name = []
-        
-        #Loop through the image folder, get, resize and normalize image
-        for directory in os.listdir(img_folder):
-            for file_path in os.listdir(os.path.join(img_folder, directory)):
-                image_path = os.path.join(img_folder, directory, file_path) #Initialize the image path
-                # Convert Image to grayscale
-                image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
-                #Resize the image
-                if(image.any()):
-                    image = cv2.resize(image, (255, 255), interpolation = cv2.INTER_AREA)
-                    #Transform image into a tensor
-                    image = np.array(image)
-                    image = image.astype('float32')
+                #normalize image
+                image = image/255.0
+                image_data_array.append(image)
+                class_name.append(directory)
+            else:
+                next
+        return image_data_array, class_name
+    
+def random_images( parent_dir, target_class, binarify=False):
+    target_folder = parent_dir+target_class
+    image = random.sample(os.listdir(target_folder))
+    plt.figure(figsize=(10,7))
+    image = plt.imread(image)
+    plt.imshow(image,cmap=plt.cm.binary)
+    
+def improve_image( path, thresh, im_bw):
+    """Improves blury or non clear image
 
-                    #normalize image
-                    image = image/255.0
-                    image_data_array.append(image)
-                    class_name.append(directory)
-                else:
-                    next
-            return image_data_array, class_name
-        
-    def random_images(self, parent_dir, target_class, binarify=False):
-        target_folder = parent_dir+target_class
-        image = random.sample(os.listdir(target_folder))
-        plt.figure(figsize=(10,7))
-        image = plt.imread(image)
-        plt.imshow(image,cmap=plt.cm.binary)
-        
-    def improve_image(self, path, thresh, im_bw):
-        """Improves blury or non clear image
+    Args:
+        path (string): The path to the image file
+        thresh (Int): thershold
+        im_bw (Int): 
+    """
+    image = cv2.imread(path)
+    image = cv2.bitwise_not(image)
+    gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray_scale = cv2.bitwise_not(gray_scale)
+    thresh,im_bw = cv2.threshold(gray_scale,thresh,im_bw, cv2.THRESH_BINARY)
+    plt.imshow(im_bw)
+    plt.show()
 
-        Args:
-            path (string): The path to the image file
-            thresh (Int): thershold
-            im_bw (Int): 
-        """
-        image = cv2.imread(path)
-        image = cv2.bitwise_not(image)
-        gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        gray_scale = cv2.bitwise_not(gray_scale)
-        thresh,im_bw = cv2.threshold(gray_scale,thresh,im_bw, cv2.THRESH_BINARY)
-        plt.imshow(im_bw)
-        plt.show()
+def generate_random_images( num:int, save_path:str):
+    """Generates and downloads random images from https://picsum.photo/200/200/?random
 
-    def generate_random_images(self, num:int, save_path:str):
-        """Generates and downloads random images from https://picsum.photo/200/200/?random
-
-        Args:
-            num (int): Number of images you wish to download
-            save_path (str): The path to which the images will be saved
-        """
-        url = "https://picsum.photo/200/200/?random"
-        for i in range(num):
-            response = requests.get(url)
-            if response.status_code == 200:
-                filename = f'img_{i}.jpg'
-                file_path = os.path.join(save_path, filename)
-                with open(file_path, 'wb') as f:
-                    print("saving: ", filename)
-                    f.write(response.content)
+    Args:
+        num (int): Number of images you wish to download
+        save_path (str): The path to which the images will be saved
+    """
+    url = "https://picsum.photo/200/200/?random"
+    for i in range(num):
+        response = requests.get(url)
+        if response.status_code == 200:
+            filename = f'img_{i}.jpg'
+            file_path = os.path.join(save_path, filename)
+            with open(file_path, 'wb') as f:
+                print("saving: ", filename)
+                f.write(response.content)
